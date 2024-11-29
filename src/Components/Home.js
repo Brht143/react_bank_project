4dc632efee96b580d7560c202531eca0de6743ef
Ibrahim <i.h.elchami@gmail.com>
2024-11-29 13:19:14 +0300

    added logout button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import { Field, Form, Formik } from "formik";
-import { checkToken } from '../API/storage'
+import { checkToken, deleteToken } from '../API/storage'
 import { deposit, getTransactions, withdraw } from '../API/user'
 import { useNavigate } from 'react-router';
 import Nav from './Nav'
@@ -7,14 +7,21 @@ import React, { useState } from 'react'
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 const Home = () => {
-
+  
+    const navigate = useNavigate()
+    
     const handleSubmit = (formData) => {
       console.log(formData.transactionSwitch)
       if (formData.transactionSwitch) depositMutation.mutate(formData)
       else withdrawMutation.mutate(formData)
     }
 
-    const navigate = useNavigate()
+    const handleLogOut = () => {
+      deleteToken()
+      navigate("/Login")
+      console.log("Log OUt")
+    }
+
 
     const depositMutation = useMutation({
             mutationFn: (formData) => deposit(formData),
@@ -38,15 +45,12 @@ const Home = () => {
           enabled: true,
         }) 
     
+
     const initialValue = 0;
     const balance = data?.map(transaction => transaction.type === "deposit" ? transaction.amount : -transaction.amount)
     .reduce((accumulator, currentValue) => accumulator + currentValue, initialValue,);
 
-
-    if (!checkToken())
-        {
-            return <navigate to="/login" />
-        }
+    if (!checkToken()) return <navigate to="/login" />
     
     return (
     <div className="main">
@@ -74,6 +78,7 @@ const Home = () => {
             <button type="submit">Submit</button>
           </Form>
         </Formik>
+            <button onClick={handleLogOut} className="logout" >Log out</button>
     </div>
   </div>
     </div>
@@ -81,4 +86,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home 
